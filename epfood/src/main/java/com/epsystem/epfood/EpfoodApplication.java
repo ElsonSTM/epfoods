f2d0faa926219d5b01a4de3875b17fa2619f60c3
Elson Pinheiro <elson.silva@ufopa.edu.br>
2021-12-06 19:04:59 -0300

    Created project

---
+++ epfood/src/main/java/com/epsystem/epfood/EpfoodApplication.java
@@ -0,0 +1,13 @@
+package com.epsystem.epfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpfoodApplication.class, args);
+	}
+
+}
---
+++ epfood/src/test/java/com/epsystem/epfood/EpfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epsystem.epfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
