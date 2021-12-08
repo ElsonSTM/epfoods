a3e75d40c29df03fd31e9cf7b1459229e863382c
Elson Pinheiro <elson.silva@ufopa.edu.br>
2021-12-08 09:30:49 -0300

    Customização Json

--- epfood/src/main/java/com/epsystem/epfood/api/controller/CozinhaController.java
+++ epfood/src/main/java/com/epsystem/epfood/api/controller/CozinhaController.java
@@ -3,7 +3,9 @@ package com.epsystem.epfood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +13,7 @@ import com.epsystem.epfood.domain.entity.Cozinha;
 import com.epsystem.epfood.domain.repository.CozinhaRepository;
 
 @RestController
-@RequestMapping("/cozinhas")
+@RequestMapping(value = "/cozinhas", produces = MediaType.APPLICATION_JSON_VALUE)
 public class CozinhaController {
 	
 	@Autowired
@@ -20,6 +22,11 @@ public class CozinhaController {
 	@GetMapping
 	public List<Cozinha> listar() {
 		return cozinhaRepository.listar();
+	}
+	
+	@GetMapping("/{cozinhaId}")
+	public Cozinha buscar(@PathVariable Long cozinhaId) {
+		return cozinhaRepository.buscar(cozinhaId);
 	}
 	
 }
--- epfood/src/main/java/com/epsystem/epfood/domain/entity/Cozinha.java
+++ epfood/src/main/java/com/epsystem/epfood/domain/entity/Cozinha.java
@@ -1,5 +1,6 @@
 package com.epsystem.epfood.domain.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -18,6 +19,9 @@ public class Cozinha {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	//@JsonIgnore
+	//@JsonProperty("titulo")
+	@Column(nullable = false)
 	private String nome;
 	
 }
