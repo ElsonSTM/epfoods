0062b7a4620627cfcdd58150fd16dc0cda14ce62
Elson Pinheiro <elson.silva@ufopa.edu.br>
2022-01-19 00:13:00 -0300

    Testando e analisando o impacto da incorporação de classe na REST API

--- epfood/src/main/resources/import.sql
+++ epfood/src/main/resources/import.sql
@@ -1,10 +1,6 @@
 insert into cozinha (id, nome) values (1, 'Brasileira');
 insert into cozinha (id, nome) values (2, 'Americana');
 
-insert into restaurante (nome, taxa_frete, cozinha_id) values ('Churrasco', 10, 1);
-insert into restaurante (nome, taxa_frete, cozinha_id) values ('Tropeiro', 9.50, 1);
-insert into restaurante (nome, taxa_frete, cozinha_id) values ('Hamburguer', 15, 2);
-
 insert into estado (id, nome) values (1, 'Minas Gerais');
 insert into estado (id, nome) values (2, 'Amazonas');
 insert into estado (id, nome) values (3, 'Pará');
@@ -16,6 +12,10 @@ insert into cidade (id, nome, estado_id) values (3, 'Belem', 3);
 insert into cidade (id, nome, estado_id) values (4, 'Santarém', 3);
 insert into cidade (id, nome, estado_id) values (5, 'Manaus', 2);
 insert into cidade (id, nome, estado_id) values (6, 'São Paulo', 4);
+
+insert into restaurante (id, nome, taxa_frete, cozinha_id, endereco_cidade_id, endereco_cep, endereco_logradouro, endereco_numero, endereco_bairro) values (1, 'Churrasco', 10, 1, 4, '68030-670', 'Trv. Arapiuns', '1481', 'Aparecida');
+insert into restaurante (id, nome, taxa_frete, cozinha_id) values (2, 'Tropeiro', 9.50, 1);
+insert into restaurante (id, nome, taxa_frete, cozinha_id) values (3, 'Hamburguer', 15, 2);
 
 insert into forma_pagamento (id, descricao) values (1, 'cartão de crédito');
 insert into forma_pagamento (id, descricao) values (2, 'cartão de débito');
