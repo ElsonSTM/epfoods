454decfe583cf5df2499d23634563d3dc6341354
Elson Pinheiro <elson.silva@ufopa.edu.br>
2022-01-19 00:33:04 -0300

    Mapeando propriedades com @CreationTimestamp e @UpdateTimestamp

--- epfood/src/main/resources/import.sql
+++ epfood/src/main/resources/import.sql
@@ -13,9 +13,9 @@ insert into cidade (id, nome, estado_id) values (4, 'Santarém', 3);
 insert into cidade (id, nome, estado_id) values (5, 'Manaus', 2);
 insert into cidade (id, nome, estado_id) values (6, 'São Paulo', 4);
 
-insert into restaurante (id, nome, taxa_frete, cozinha_id, endereco_cidade_id, endereco_cep, endereco_logradouro, endereco_numero, endereco_bairro) values (1, 'Churrasco', 10, 1, 4, '68030-670', 'Trv. Arapiuns', '1481', 'Aparecida');
-insert into restaurante (id, nome, taxa_frete, cozinha_id) values (2, 'Tropeiro', 9.50, 1);
-insert into restaurante (id, nome, taxa_frete, cozinha_id) values (3, 'Hamburguer', 15, 2);
+insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, endereco_cidade_id, endereco_cep, endereco_logradouro, endereco_numero, endereco_bairro) values (1, 'Churrasco', 10, 1, utc_timestamp, utc_timestamp, 4, '68030-670', 'Trv. Arapiuns', '1481', 'Aparecida');
+insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao) values (2, 'Tropeiro', 9.50, 1, utc_timestamp, utc_timestamp);
+insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao) values (3, 'Hamburguer', 15, 2, utc_timestamp, utc_timestamp);
 
 insert into forma_pagamento (id, descricao) values (1, 'cartão de crédito');
 insert into forma_pagamento (id, descricao) values (2, 'cartão de débito');
